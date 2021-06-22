c85f16332ad6ce34ffbae1b8f038dd78b8f4b767
amaa1981 <amaa_1981@hotmail.com>
2021-06-22 09:55:34 +0300

    v1

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+        <title>Deploy with OpenShift</title>
+    </head>
+    <body>
+        <?php
+echo "<h2>Hello from Red Hat v3!</h2>"; 
+?>
+        <div>
+            <img src="rh.png" alt="myPic" />
+        </div>
+    </body>
+</html>
